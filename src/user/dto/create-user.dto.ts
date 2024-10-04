45c53f52cda342479287ec0b6834b2cad7d5c9ec
Jose Ulisses <joseulisses03@gmail.com>
2024-10-04 18:11:27 -0300

    Adding swagger documentation for users resources

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,4 +1,8 @@
+import { ApiProperty } from "@nestjs/swagger"
+
 export class CreateUserDto {
+    @ApiProperty()
     email: string
+    @ApiProperty()
     password: string
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,12 +2,17 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, HttpException, HttpS
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('users')
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
+  @ApiResponse({status: 201, description: `The user was created`})
+  @ApiResponse({status: 400, description: `Missing required fields in the requisiton body`})
+  @ApiResponse({status: 409, description: `The e-mail already exists in the database`})
   async create(@Body() createUserDto: CreateUserDto) {
     try {
       if (!createUserDto.email) {
@@ -31,20 +36,38 @@ export class UserController {
     return await this.userService.findAll();
   }
 
-  @Get(':email')
-  async findByEmail(@Param('email') email: string) {
-     return await this.userService.findByEmail(email)
-  }
-
+  @ApiResponse({status: 200, description: `Success`})
+  @ApiResponse({status: 404, description: `There is no user with given id in the database`})
   @Get(':id')
   async findOne(@Param('id') id: string) {
-    const user = await this.userService.findOne(+id);
-    if (!user) {
-      throw new HttpException(`No user found for the id ${id}`, HttpStatus.NOT_FOUND)
+    try {
+      const user = await this.userService.findOne(+id)
+      if (!user) {
+        throw new HttpException(`No user found for the id ${id}`, HttpStatus.NOT_FOUND)
+      }
+      return user
+    } catch (error) {
+      throw error
     }
-    return user;
   }
 
+  @ApiResponse({status: 200, description: `Success`})
+  @ApiResponse({status: 404, description: `There is no user with given email in the database`})
+  @Get('/findByEmail/:email')
+  async findByEmail(@Param('email') email: string) {
+     try {
+        const user = await this.userService.findByEmail(email)
+        if (!user) {
+          throw new HttpException(`There is no user with given email ${email} in the database`, HttpStatus.NOT_FOUND)
+        }
+        return user
+     } catch (error) {
+          throw error
+     }
+  }
+
+  @ApiResponse({status: 200, description: `Successfully updated user`})
+  @ApiResponse({status: 404, description: `There is no user with given id in the database`})
   @Patch(':id')
   async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     const user = await this.userService.findOne(+id);
@@ -54,6 +77,8 @@ export class UserController {
     return await this.userService.update(+id, updateUserDto);
   }
 
+  @ApiResponse({status: 200, description: `Successfully deleted user`})
+  @ApiResponse({status: 404, description: `There is no user with given id in the database`})
   @Delete(':id')
   async remove(@Param('id') id: string) {
     const user = await this.userService.findOne(+id)
